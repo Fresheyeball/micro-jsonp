ecb4c6c64df9e9edba06b10901833ed5d3e523aa
Kyle Kress <savedforarainyday@yahoo.com>
2016-05-31 15:36:39 -0600

    zopfli to the rescue

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
+#!/usr/bin/env bash
+
 uglifyjs -m -- src/jsonp.js > src/jsonp.min.js
-gzip -1 -c src/jsonp.min.js > src/jsonp.min.js.gz
+
+# use zopfli for better compression if available
+if type "zopfli" &> /dev/null; then
+  zopfli --gzip --i100 src/jsonp.min.js
+else
+  gzip -1 -c src/jsonp.min.js > src/jsonp.min.js.gz
+fi
